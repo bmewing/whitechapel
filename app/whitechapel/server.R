bb4a0cae539ebfbeddf930e108fd08c6c0c9a30a
Ewing <u772700@emn.com>
2018-07-23 12:55:13 -0400

    migrated package code and updated shiny app

--- app/whitechapel/server.R
+++ app/whitechapel/server.R
@@ -1,115 +1,16 @@
 library(shiny)
 library(igraph)
 library(shinyjs)
+library(whitechapelR)
 load("node_locations.rda")
 load("roads.rda")
 load("alley.rda")
 
-start_round = function(initial_murder){
-  #' @title Start a new round
-  #' 
-  #' @description Generate the initial list for a new round
-  #' 
-  #' @param initial_murder integer Space of the initial murder(s)
-  
-  l = list()
-  l[[1]] = initial_murder[1]
-  if(length(initial_murder) == 2) l[[2]] = initial_murder[2]
-  return(l)
-}
-
-#' export
-
-take_a_step = function(paths,roads){
-  paths = lapply(paths,gen_possibilities,roads=roads)
-  return(unlist(paths,recursive = FALSE))
-}
-
-#' export
-
-take_a_carriage = function(paths,roads){
-  paths = take_a_step(paths,roads)
-  paths = take_a_step(paths,roads)
-  return(paths)
-}
-
-gen_possibilities = function(path,roads){
-  start = rev(path)[1]
-  p = unique(unlist(roads[roads$x == start | roads$y == start,]))
-  p = p[p != start]
-  return(lapply(p,add_possibilities,path=path))
-}
-
-add_possibilities = function(p,path){
-  return(c(path,p))
-}
-
-end_round = function(paths,hideouts=NULL){
-  possible_hideouts = lapply(paths,function(x){
-    rev(x)[1]
-  })
-  possible_hideouts = unique(unlist(possible_hideouts))
-  if(is.null(hideouts)) return(sort(possible_hideouts))
-  hideouts = intersect(hideouts,possible_hideouts)
-  return(sort(hideouts))
-}
-
-inspect_space = function(paths,space,clue){
-  if(clue){
-    paths = lapply(paths,found_clue,space=space)
-  } else {
-    paths = lapply(paths,found_nothing,space=space)
-  }
-  return(plyr::compact(paths))
-}
-
-found_clue = function(path,space){
-  if(!any(path %in% space)) return(NULL)
-  return(path)
-}
-
-found_nothing = function(path,space){
-  if(any(path %in% space)) return(NULL)
-  return(path)
-}
-
-show_board = function(paths=NULL,hideouts=NULL){
-  r = roads
-  a = alley
-  r$lty = 1
-  a$lty = 3
-  r$weight = 1
-  a$weight = 1
-  l = as.matrix(node_locations[order(node_locations$name),c("x","y")])
-  l[,2] = l[,2]*-1
-  v = data.frame(name = 1:195,cex=0.7,color="white",stringsAsFactors = FALSE)
-  g = igraph::graph_from_data_frame(rbind(r,a),directed = FALSE,vertices = v)
-  par(mai=c(0,0,0,0))
-  if(!is.null(paths)){
-    colors = c("#FFD9D9","#F5C5C5","#ECB1B1","#E39D9D","#D98A8A","#D07676","#C76262","#BE4E4E","#B43B3B","#AB2727","#A21313","#990000")
-    tbl = table(unlist(paths))
-    to_replace = colors[round(tbl/max(tbl)*10,0)]
-    col = V(g)$color
-    col[as.numeric(names(tbl))] = to_replace
-    V(g)$color = col
-  }
-  if(!is.null(hideouts)){
-    shapes = rep("circle",195)
-    shapes[hideouts] = "square"
-    V(g)$shape = shapes
-    col = V(g)$color
-    col[hideouts] = "sky blue"
-    V(g)$color = col
-  }
-  plot(g,layout=l,vertex.size = 5)
-}
-
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
    
-  values = reactiveValues(paths=NULL,hideouts=NULL)
+  values = reactiveValues(paths=NULL,hideouts=NULL,blocked=NULL)
   
   observe({
-    print(length(input$initialMurder))
     if(!is.null(input$initialMurder)){
       if(length(input$initialMurder) <= 2) enable("newRound") else disable("newRound")
     } 
@@ -137,7 +38,10 @@ shinyServer(function(input, output) {
   })
   
   observeEvent(input$basicMove,{
-    values$paths = take_a_step(values$paths,roads)
+    print(purrr::vec_depth(values$paths))
+    z <<- values$blocked
+    pth <<- values$paths
+    values$paths = take_a_step(values$paths,roads,values$blocked)
   })
   
   observeEvent(input$carriageMove,{
@@ -155,8 +59,35 @@ shinyServer(function(input, output) {
                                  ,clue = result)
   })
   
-  output$map = renderPlot({
-    show_board(values$paths,values$hideouts)
+  observeEvent(input$addBlock,{
+    bm = as.numeric(gsub(" +","",strsplit(input$blockedMovement,"[, ]+")[[1]]))
+    if(length(bm) >= 2){
+      if(length(bm) %% 2 == 1) bm = bm[-length(bm)]
+      bp = lapply(seq(1,length(bm),by=2),function(x){
+        bm[x:(x+1)]
+      })
+      values$blocked = unique(c(values$blocked,bp))
+    }
+    updateTextInput(session,inputId = "blockedMovement",value = "")
   })
   
+  observeEvent(input$clearBlock,{
+    print('trying to clear!')
+    values$blocked <- NULL
+  })
+  
+  output$map = renderPlot({
+    show_board(values$paths,values$hideouts,roads,alley,node_locations)
+  })
+  
+  output$blockedPairs = renderTable({
+    if(!is.null(values$blocked)){
+      tbl = do.call(rbind,values$blocked)
+      tbl[,1] = as.character(tbl[,1])
+      tbl[,2] = as.character(tbl[,2])
+    } else {
+      tbl = NULL
+    }
+    tbl
+  },colnames=FALSE)
 })
--- app/whitechapel/ui.R
+++ app/whitechapel/ui.R
@@ -30,6 +30,24 @@ shinyUI(
                     ,label="Alleyway"
                     ,icon=icon("building")
                     ,class="btn btn-lg btn-danger"),
+        br(),
+        br(),
+        h3("Movement Restriction"),
+        "Here is where you can specify illegal movement for Jack by noting nodes which are blocked by policemen.",
+        "Type node numbers by separating them with a space or a comma.",
+        "If more than two nodes are input, they will processed as pairs.",
+        br(),
+        br(),
+        textInput("blockedMovement","Blocked Movement",placeholder = "50,31,30,13"),
+        actionButton("addBlock"
+                    ,label="Add Block"
+                    ,icon=icon("ban")
+                    ,class="btn btn-lg btn-danger"),
+        actionButton("clearBlock"
+                    ,label="Clear Blocks"
+                    ,icon=icon("unlock")
+                    ,class="btn btn-lg btn-danger"),
+        tableOutput("blockedPairs"),
         hr(),
         h3("Investigation Control"),
         selectInput("spaceInvestigated","Space Investigated",choices=1:195,multiple=TRUE),
